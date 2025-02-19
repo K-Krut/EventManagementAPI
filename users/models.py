7f4f1fe489cdab78f48816bbd2ec13de3b3fb0db
K-Krut <katiakrut448@gmail.com>
2025-02-19 16:23:22 +0200

    user model changed & settings updated

--- users/models.py
+++ users/models.py
@@ -37,7 +37,7 @@ class User(AbstractUser, PermissionsMixin):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_notifications = models.BooleanField(default=False)
-    avatar = models.ImageField(upload_to="users/profile-pictures", blank=True, null=True)
+    # avatar = models.ImageField(upload_to="users/profile-pictures", blank=True, null=True)
     registration_date = models.DateTimeField(auto_now_add=True)
 
     is_staff = models.BooleanField(default=False)
