89f333bf1edc313b95aedcffc705e4778dfcb9cb
K-Krut <katiakrut448@gmail.com>
2025-02-19 14:21:36 +0200

    user model completed

--- users/models.py
+++ users/models.py
@@ -32,7 +32,6 @@ class CustomUserManager(BaseUserManager):
         return user
 
 
-
 class User(AbstractUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=100)
@@ -44,3 +43,9 @@ class User(AbstractUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
