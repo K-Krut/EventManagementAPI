27d1628ccd1bd223cc2139cf728c466f04414980
K-Krut <katiakrut448@gmail.com>
2025-02-19 20:50:30 +0200

    user login serializer added

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
 
@@ -28,3 +31,43 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+        print(email, password)
+
+        if not email:
+            raise ValidationError('Email is required')
+        if not password:
+            raise ValidationError('Password is required')
+
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise ValidationError('Invalid email or password')
+
+        data['user'] = user
+        return data
+
+    def create(self, validated_data):
+        user = validated_data['user']
+        print(user)
+        tokens = RefreshToken.for_user(user)
+
+        return {
+            'access_token': str(tokens.access_token),
+            'refresh_token': str(tokens),
+            'user': {
+                'id': user.id,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email_notifications': user.email_notifications
+            }
+        }
