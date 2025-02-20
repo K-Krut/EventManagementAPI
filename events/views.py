926433308274487068d582c59ef55ab5cd8dcf9e
K-Krut <katiakrut448@gmail.com>
2025-02-20 18:27:09 +0200

    /events/my/ endpoint added (EventMyListView) with possibility to sort and filter data

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework import filters, exceptions
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from events.models import Event
@@ -30,8 +31,22 @@ class EventListView(generics.ListCreateAPIView):
 
 
 class EventMyListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EventSerializer
+    pagination_class = EventPagination
+    ordering_fields = ['title', 'date_start', 'date_end', 'status', 'type']
+    ordering = ['-date_start', 'status', 'type']
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title']
+
     def get_queryset(self):
-        pass
+        user = self.request.user
+        try:
+            queryset = Event.objects.filter(organizer=user) | Event.objects.filter(event__user=user)
+            return queryset.distinct().order_by(*self.ordering)
+        except Exception as error:
+            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class EventDetailView(generics.ListCreateAPIView):
