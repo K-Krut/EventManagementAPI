6e11919aea21e094a46cff7d0e64071d6c1bc8e4
K-Krut <katiakrut448@gmail.com>
2025-02-20 18:00:19 +0200

    updated event serializer (added am i already registered to the event logic & updated organizer response structure) & added ParticipantSerializer

--- events/serializers.py
+++ events/serializers.py
@@ -1,13 +1,28 @@
 from rest_framework import serializers
 
-from events.models import Event
+from events.models import Event, EventParticipants
+from users.models import User
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name']
 
 
 class EventSerializer(serializers.ModelSerializer):
+
+    organizer = ParticipantSerializer(read_only=True)
+    is_registered = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
         fields = [
             'id', 'title', 'slug', 'date_start', 'date_end', 'description', 'is_online', 'location', 'status', 'type',
-            'organizer'
+            'is_registered', 'organizer'
         ]
+
+    def get_is_registered(self, obj):
+        user = self.context['request'].user
+
+        return False if not user.is_authenticated else EventParticipants.objects.filter(user=user, event=obj).exists()
