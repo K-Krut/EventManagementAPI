a2f6d1f366a8f90426011749286cd43a51aacbad
K-Krut <katiakrut448@gmail.com>
2025-02-20 15:44:00 +0200

    added routes and base view classes for get method

--- events/views.py
+++ events/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+class EventListView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        pass
+
+
+class EventMyListView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        pass
+
+
+class EventDetailView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        pass
+
+
+class EventParticipantsView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        pass
